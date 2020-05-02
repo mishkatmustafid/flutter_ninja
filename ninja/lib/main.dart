8de39fd02c7792cdce756a31b411f1f8aa9fed77
mishkatmustafid <mishkatmustafid@gmail.com>
2020-05-02 15:52:29 +0600

    Initial

---
+++ ninja/lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(
+  home: NinjaCard(),
+));
+
+class NinjaCard extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
